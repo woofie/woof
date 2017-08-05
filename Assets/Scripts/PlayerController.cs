03f88c9fe511440443ae73fc15498b8742b8d649
Jackson Huang <jackson_huang@live.com>
2017-08-05 21:48:40 +1000

    moving ball is working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+	public GameObject dog;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		dog.GetComponent<dog> ().FollowBallToggleOn (this.dog);
+	}
+
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = CrossPlatformInputManager.GetAxis ("Horizontal");
+		float moveVertical = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
--- Assets/dog.cs
+++ Assets/dog.cs
@@ -7,33 +7,26 @@ public class dog : MonoBehaviour {
 
 	private Rigidbody rb;
 	private Animation anim;
+	private GameObject ball;
+	private bool followBall;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		anim = GetComponent<Animation> ();
+		followBall = false;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		float x = CrossPlatformInputManager.GetAxis ("Horizontal");
-		float y = CrossPlatformInputManager.GetAxis ("Vertical");
-
-		Vector3 movement = new Vector3 (x, 0, y);
-
-		rb.velocity = movement * 4f;
-
-		if (x != 0 && y != 0) {
-			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, Mathf.Atan2 (x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
-
+	void Update ()
+	{
+		if (followBall) {
+			transform.position = Vector3.MoveTowards (transform.position, ball.transform.position, .03f);
 		}
+	}
 
-		if (x != 0 || y != 0) {
-			anim.Play ("CorgiWalk");
-
-		} else {
-			anim.Play ("CorgiDeath");
-		}
-
+	public void FollowBallToggleOn (GameObject ball){
+		followBall = true;
+		this.ball = ball;
 	}
 }
