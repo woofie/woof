8ec6fbcc7daa3e48341355ec13ad26b518631664
Jackson Huang <jackson_huang@live.com>
2017-08-06 07:49:30 +1000

    call chasing

--- Assets/dog.cs
+++ Assets/dog.cs
@@ -7,33 +7,50 @@ public class dog : MonoBehaviour {
 
 	private Rigidbody rb;
 	private Animation anim;
+	public Transform target;
+	private bool followBall;
+	public float RotationSpeed;
+
+	private Quaternion _lookRotation;
+	private Vector3 _direction;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		anim = GetComponent<Animation> ();
+		followBall = false;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		float x = CrossPlatformInputManager.GetAxis ("Horizontal");
-		float y = CrossPlatformInputManager.GetAxis ("Vertical");
+	void Update ()
+	{
+		if (true) {// followBall) {
+			var dist = Vector3.Distance (transform.position, target.transform.position);
+			float rate = 0.0f;
+			if (dist > 3) {
+				anim.CrossFade ("CorgiRun");
+				rate = 0.1f;
+			} else if (dist > 1) {
+				anim.CrossFade ("CorgiWalk");
+				rate = 0.03f;
+			} else if (dist >= 0) {
+				anim.CrossFade ("CorgiSitScratch");
+			}
+			transform.position = Vector3.MoveTowards (transform.position, target.transform.position, rate);
 
-		Vector3 movement = new Vector3 (x, 0, y);
+			_direction = (target.position - transform.position).normalized;
 
-		rb.velocity = movement * 4f;
+			//create the rotation we need to be in to look at the target
+			_lookRotation = Quaternion.LookRotation(_direction);
 
-		if (x != 0 && y != 0) {
-			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, Mathf.Atan2 (x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
-
+			//rotate us over time according to speed until we are in the required rotation
+			transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
 		}
 
-		if (x != 0 || y != 0) {
-			anim.Play ("CorgiWalk");
 
-		} else {
-			anim.Play ("CorgiDeath");
-		}
+	}
 
+	public void FollowBallToggleOn (){
+		followBall = true;
 	}
 }
